334aad250b9afedde7e8d904cc824b2cc9eb185d
Robin Glen <robin.glen@net-a-porter.com>
2017-07-18 15:42:48 +0100

    add in client perf itorator

--- example/client.js
+++ example/client.js
@@ -4,9 +4,21 @@ const { client } = require('../src');
 const NAP_PRODUCT_API =
   'https://api.net-a-porter.com/NAP/GB/en/10/0/summaries/expand?customListUrlKeys=whats-new-this-month';
 
+// Example showing how you could collect performance metrics to work out average of API
 async function runPerformanceTest() {
-  const results = await client.performanceTestApi(NAP_PRODUCT_API);
-  console.log(results);
+  const collectionOfPerformanceMetrics = [];
+
+  for (let i = 0; i < 10; i++) {
+    const cacheBuster = Math.random();
+    collectionOfPerformanceMetrics.push(
+      await client.performanceTestApi(
+        `${NAP_PRODUCT_API}&cacheBuster=${cacheBuster}`
+      )
+    );
+  }
+
+  console.log(collectionOfPerformanceMetrics);
+  process.exit(0);
 }
 
 client.framework.listen(3000, () => {
